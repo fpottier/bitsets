18e0558f576d4ba0ee12582a1339c6d3043f5eb7
François Pottier <francois.pottier@inria.fr>
2025-11-10 15:00:05 +0100

    test/LeftistHeap: fix the test of [pop2].
    The previous test did not verify that the elements extracted by the
    candidate implementation were actually minimal.

--- test/LeftistHeap/test.ml
+++ test/LeftistHeap/test.ml
@@ -10,12 +10,28 @@
 (*                                                                            *)
 (******************************************************************************)
 
+let format = PPrint.utf8format
 open Monolith
+let valid x = Valid x
+let invalid f = Invalid f
+
+(* We use integer keys and values. *)
+module Key = Int
+module Val = Int
+module KVPair = struct
+  type t = Key.t * Val.t
+  let show (k, v) =
+    Printf.sprintf "(%d, %d)" k v
+  let equal (k, v) (k', v') =
+    Key.equal k k' && Val.equal v v'
+  let leq (k, _) (k', _) =
+    Key.compare k k' <= 0
+end
 
 (* This is the candidate implementation. *)
 module C = struct
 
-  include Bitsets.LeftistHeap.Make(Int)
+  include Bitsets.LeftistHeap.Make(Key)
 
   (* To test [pop2], we define [pop2'], a wrapper whose type is simpler. *)
 
@@ -33,64 +49,83 @@ end
 (* This is the reference implementation. *)
 module R = struct
 
-  include Reference.Make(Int)
+  include Reference.Make(Key)
 
-  let format =
-    PPrint.utf8format
+  exception Absent of KVPair.t
 
-  let show (k, v) =
-    Printf.sprintf "(%d, %d)" k v
+  (* [remove kv kvs] removes the key-value pair [kv] from the list [kvs].
+     If [kv] does not appear in the list [kvs], [Absent kv] is raised. *)
 
-  let rec remove (k, v) kvs =
+  let rec remove kv kvs =
     match kvs with
     | [] ->
-        raise Not_found
-    | (k', v') :: kvs ->
-        if k = k && v = v' then kvs else (k', v') :: remove (k, v) kvs
+        raise (Absent kv)
+    | kv' :: kvs ->
+        if KVPair.equal kv kv' then kvs else kv' :: remove kv kvs
 
-  (* Because [pop2] is non-deterministic, our reference implementation
-     of [pop2'] must validate and simulate the result of the candidate
-     implementation. *)
+  (* [check_minimal kv kvs] checks that the key-value pair [kv] is minimal
+     with respect to the list [kvs], that is, every key in the list is greater
+     than or equal to the key in the pair [kv]. If this is not the case,
+     [NotMinimal kv] is raised. *)
+
+  let is_minimal kv kvs =
+    List.for_all (KVPair.leq kv) kvs
+
+  exception NotMinimal of KVPair.t
+
+  let check_minimal kv kvs =
+    if not (is_minimal kv kvs) then
+      raise (NotMinimal kv)
+
+  let remove_minimal kv kvs =
+    let kvs' = remove kv kvs in (* may raise [Absent] *)
+    check_minimal kv kvs;       (* may raise [NotMinimal] *)
+    kvs'
+
+  (* [handle action] runs [action()] and handles the exception [Absent]
+     and [NotMinimal] by returning an [Invalid] diagnostic. *)
+
+  let handle (action : unit -> 'a diagnostic) : 'a diagnostic =
+    try
+      action()
+    with
+    | Absent kv ->
+        invalid @@ fun _doc ->
+        format "(* candidate returns %s, which does not exist *)"
+          (KVPair.show kv)
+    | NotMinimal kv ->
+        invalid @@ fun _doc ->
+        format "(* candidate returns %s, which is not minimal *)"
+          (KVPair.show kv)
+
+  (* Because [pop2] is non-deterministic, our reference implementation of
+     [pop2'] must validate and simulate the result of the candidate
+     implementation. To do so, we must check that the key-value pair [kv]
+     chosen by the candidate exists in the reference implementation and is
+     minimal. *)
 
   let pop2' (q : 'a heap) (result : (int * 'a) list * 'a C.heap) =
     let kvs, _cq = result in
     match kvs with
     | [kv1; kv2] ->
-        begin match remove kv1 (remove kv2 q) with
-        | q ->
-            Monolith.Valid ([kv1; kv2], q)
-        | exception Not_found ->
-            let cause _doc =
-              format "(* candidate returns [%s; %s]; at least one of these pairs does not exist *)"
-                (show kv1) (show kv2)
-            in
-            Monolith.Invalid cause
-        end
+        handle @@ fun () ->
+        let q = remove_minimal kv1 q in
+        let q = remove_minimal kv2 q in
+        valid (kvs, q)
     | [kv1] ->
-        begin match remove kv1 q with
-        | q ->
-            if q = [] then
-              Monolith.Valid ([kv1], q)
-            else
-              let cause _doc =
-                format "(* candidate returns only one element; there are more *)"
-              in
-              Monolith.Invalid cause
-        | exception Not_found ->
-            let cause _doc =
-              format "(* candidate returns [%s]; this pair does not exist *)"
-                (show kv1)
-            in
-            Monolith.Invalid cause
-        end
+        handle @@ fun () ->
+        let q = remove_minimal kv1 q in
+        if q = [] then
+          valid (kvs, q)
+        else
+          invalid @@ fun _doc ->
+          format "(* candidate returns only one element; there are more *)"
     | [] ->
         if q = [] then
-          Monolith.Valid ([], q)
+          valid (kvs, q)
         else
-          let cause _doc =
-            format "(* candidate returns zero element; there are some *)"
-          in
-          Monolith.Invalid cause
+          invalid @@ fun _doc ->
+          format "(* candidate returns zero element; there are some *)"
     | _ ->
         (* [pop2'] never returns more than two elements *)
         assert false
